9ec1aad6fbce30361d1365c66af643747165a228
macskamaffia <utalomfriccset@gmail.com>
2025-09-03 19:35:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 // Firebase config – saját adatokkal helyettesítsd
-const firebaseConfig = {
-  apiKey: "AIzaSyAb3h37qh_4rm9J9lbxRMwNMApWo_ExxXI",
-  authDomain: "tuzserlege-3e50f.firebaseapp.com",
-  databaseURL: "https://tuzserlege-3e50f-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "tuzserlege-3e50f",
-  storageBucket: "tuzserlege-3e50f.firebasestorage.app",
-  messagingSenderId: "607271028125",
-  appId: "1:607271028125:web:002093b6cd81c6f6eadb74"
-};
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
 
 const ADMIN_HASH = "f10e2821bbbea527ea02200352313bc059445190"; // hash: 'trimagus2025'
 
